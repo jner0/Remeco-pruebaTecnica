a2c4c13072a057bdd2db4b6f4d0b82a4ed6b717f
Nemecio Rodriguez <jnemecio10@gmail.com>
2023-03-29 23:38:28 -0500

    Modificando Formulario

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -18,7 +19,9 @@ import { NotFoundComponent } from './components/not-found/not-found.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/form/form.component.ts
+++ src/app/components/form/form.component.ts
@@ -1,10 +1,48 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-form',
   templateUrl: './form.component.html',
   styleUrls: ['./form.component.css']
 })
-export class FormComponent {
+export class FormComponent implements OnInit{
+
+  formModel: FormGroup;
+
+  constructor(){
+    this.formModel = new FormGroup({
+      placa: new FormControl('',[
+        Validators.required,
+      ]),
+
+      fecha: new FormControl('',[
+        Validators.required
+      ]),
+
+      hora: new FormControl('', [
+        Validators.required
+      ])
+
+      
+    }, [])
+  }
+
+  ngOnInit(): void {
+
+  }
+
+  getDataForm(){
+    console.log(this.formModel.value)
+  }
+
+
+  checkControl(pControlName: string, pError: string) : boolean{
+    if(this.formModel.get(pControlName)?.hasError(pError) && this.formModel.get(pControlName)?.touched){
+      return true
+    }else{
+      return false
+    }
+  }
 
 }
