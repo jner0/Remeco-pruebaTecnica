3cf74b2d1419a41a5a2c080fc27d9a9f3f275c42
Nemecio Rodriguez <jnemecio10@gmail.com>
2023-03-29 22:46:32 -0500

    Creando rutas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FormComponent } from './components/form/form.component';
+import { HomeComponent } from './components/home/home.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path:"", pathMatch:"full", redirectTo:"home" },
+  { path:"home", component: HomeComponent },
+  { path:"nuevaconsulta", component: FormComponent },
+  { path:"**", component: NotFoundComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
