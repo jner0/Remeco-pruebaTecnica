e41554810e36d43e95fb391c54376201a75bdf99
Nemecio Rodriguez <jnemecio10@gmail.com>
2023-03-30 12:02:12 -0500

    Correcion en condicional

--- src/app/components/form/form.component.ts
+++ src/app/components/form/form.component.ts
@@ -42,12 +42,17 @@ export class FormComponent implements OnInit{
     // Verificamos si la fecha corresponde a un día en el que aplica el Pico y Placa
     const diaSemana = new Date(fecha);
     const diaEnNumero = diaSemana.getDay(); //transforma el dia en un numero del 0 al 6 siendo lunes = 0 y asi sucesivamente.
-    const aplicaPicoPlaca: boolean = ((diaEnNumero >= 1 && diaEnNumero <= 5) || diaEnNumero == 0) && hora >= '07:00' && hora <= '09:30' || hora >= '16:00' && hora <= '19:30';
+    const aplicaPicoPlaca: boolean = ((diaEnNumero >= 1 && diaEnNumero <= 5) || diaEnNumero == 0) && hora >= '07:00' && hora <= '09:30' || hora >= '16:00' && hora <= '21:00';
     
     // Verificamos si la placa está restringida para circular en la fecha y hora especificadas
-    const puedeCircular: boolean = 
-    (ultimoDigitoPlaca % 2 == 0 && diaEnNumero == 1 || ultimoDigitoPlaca % 2 == 0 && diaEnNumero == 3 || ultimoDigitoPlaca % 2 == 0 && diaEnNumero == 5) || 
-    (ultimoDigitoPlaca % 2 == 1 && diaEnNumero == 2 || ultimoDigitoPlaca % 2 == 1 && diaEnNumero == 4 || ultimoDigitoPlaca % 2 == 1 && diaEnNumero == 0);
+    let puedeCircular = true;
+    if ((diaEnNumero === 0 && (ultimoDigitoPlaca === 1 || ultimoDigitoPlaca === 2)) ||
+        (diaEnNumero === 1 && (ultimoDigitoPlaca === 3 || ultimoDigitoPlaca === 4)) ||
+        (diaEnNumero === 2 && (ultimoDigitoPlaca === 5 || ultimoDigitoPlaca === 6)) ||
+        (diaEnNumero === 3 && (ultimoDigitoPlaca === 7 || ultimoDigitoPlaca === 8)) ||
+        (diaEnNumero === 4 && (ultimoDigitoPlaca === 9 || ultimoDigitoPlaca === 0))) {
+        puedeCircular = false;
+  }
 
     // Construimos el mensaje de acuerdo al resultado de la verificación
     let mensaje: string = "";
